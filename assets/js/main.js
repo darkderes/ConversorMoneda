1d4ce19850c29ad2fd3973ee7f964e8895e2edb8
Jorge Darderes <JDARDERES@hotmail.com>
2025-01-07 17:18:46 -0300

    feat: agregar funcionalidad para renderizar gráficos de cambio de moneda

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,6 +24,8 @@ const renderDOM = (data) => {
   document.getElementById(
     "result"
   ).textContent = `RESULTADO : ${amount} ${fromCurrency} = ${convertedAmount} ${toCurrency}`;
+
+  //   renderGrafica("euro");
 };
 
 const getApiData = async () => {
@@ -38,3 +40,37 @@ document
     e.preventDefault();
     getApiData();
   });
+
+async function getAndCreateDataToChart(money) {
+  const res = await getApi(`https://www.mindicador.cl/api/${money}`);
+  const serie = res.serie.slice(0, 10);
+  const labels = serie.map((ser) => {
+    return ser.fecha;
+  });
+  console.log(labels);
+
+  const data = serie.map((mon) => {
+    return Number(mon.valor);
+  });
+  console.log(data);
+  const datasets = [
+    {
+      label: `Cambio ${money}`,
+      borderColor: "rgb(29, 134, 29)",
+      data,
+    },
+  ];
+  return { labels, datasets };
+}
+
+async function renderGrafica(money) {
+  const data = await getAndCreateDataToChart(money);
+  const config = {
+    type: "line",
+    data,
+  };
+  const myChart = document.getElementById("myChart");
+  myChart.style.backgroundColor = "#333";
+  new Chart(myChart, config);
+}
+renderGrafica("euro");
