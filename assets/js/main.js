58f1560fb306786f7db3a65ae34a56524c687a63
Jorge Darderes <JDARDERES@hotmail.com>
2025-01-10 09:34:54 -0300

    fix: corregir formato de fecha en los gráficos de cambio de moneda

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,7 @@ const renderDOM = (data) => {
   const toCurrency = document.getElementById("to-currency").value;
   const convertedAmount = (amount / data.dolar.valor).toFixed(2);
   const value = toCurrency === "USD" ? data.dolar.valor : data.euro.valor;
+  const money = toCurrency === "USD" ? "dolar" : "euro";
 
   fuente.textContent = `Fuente : ${data.autor}`;
 
@@ -25,7 +26,7 @@ const renderDOM = (data) => {
     "result"
   ).textContent = `RESULTADO : ${amount} ${fromCurrency} = ${convertedAmount} ${toCurrency}`;
 
-  //   renderGrafica("euro");
+  renderGrafica(money);
 };
 
 const getApiData = async () => {
@@ -41,36 +42,71 @@ document
     getApiData();
   });
 
+let myChartInstance;
+
 async function getAndCreateDataToChart(money) {
   const res = await getApi(`https://www.mindicador.cl/api/${money}`);
-  const serie = res.serie.slice(0, 10);
-  const labels = serie.map((ser) => {
-    return ser.fecha;
-  });
-  console.log(labels);
+  let serie = res.serie.slice(0, 10);
+  // Ordenar las fechas de menor a mayor
+  serie = serie.sort((a, b) => new Date(a.fecha) - new Date(b.fecha));
+  const labels = serie.map((ser) => formatDate(ser.fecha));
+  const data = serie.map((mon) => Number(mon.valor));
 
-  const data = serie.map((mon) => {
-    return Number(mon.valor);
-  });
-  console.log(data);
-  const datasets = [
-    {
-      label: `Cambio ${money}`,
-      borderColor: "rgb(29, 134, 29)",
-      data,
-    },
-  ];
-  return { labels, datasets };
+  return {
+    labels,
+    datasets: [
+      {
+        label: `Cambio ${money}`,
+        borderColor: "rgb(29, 134, 29)",
+        data,
+      },
+    ],
+  };
 }
+
+const formatDate = (fecha) => {
+  const date = new Date(fecha);
+  return date.toLocaleDateString("es-ES");
+};
 
 async function renderGrafica(money) {
   const data = await getAndCreateDataToChart(money);
-  const config = {
+  const config = createChartConfig(data);
+  const myChart = document.getElementById("myChart");
+
+  // Destruir el gráfico existente si existe
+  if (myChartInstance) {
+    myChartInstance.destroy();
+  }
+
+  myChart.style.backgroundColor = "#333";
+  myChartInstance = new Chart(myChart, config);
+}
+
+function createChartConfig(data) {
+  return {
     type: "line",
     data,
+    options: {
+      scales: {
+        x: {
+          ticks: {
+            color: "white", // Color de las letras en el eje x
+          },
+        },
+        y: {
+          ticks: {
+            color: "white", // Color de las letras en el eje y
+          },
+        },
+      },
+      plugins: {
+        legend: {
+          labels: {
+            color: "white", // Color de las letras en la leyenda
+          },
+        },
+      },
+    },
   };
-  const myChart = document.getElementById("myChart");
-  myChart.style.backgroundColor = "#333";
-  new Chart(myChart, config);
 }
-renderGrafica("euro");
